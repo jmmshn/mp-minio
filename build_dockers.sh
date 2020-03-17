0ef347965e7e053d759fcea5785e83a40b66d31f
jmmshn <jmmshn@gmail.com>
2020-03-17 15:08:05 -0700

    updated

--- build_dockers.sh
+++ build_dockers.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+read -p "version tag: " ver
+echo
 
+tag=v$(date +"%F")
+if [ -n "$ver" ]
+then
+  tag="${tag}_$ver"
+else
+  tag="latest"
+fi
+echo "Tag: $tag"
 root_dir=$(pwd)
 cd app
-docker build .
-
+docker image build --tag registry.spin.nersc.gov/perssongroup/mp-minio-app:$tag .
+docker image push registry.spin.nersc.gov/perssongroup/mp-minio-app:$tag
 cd $root_dir
 cd web
-docker build .
-
+docker image build --tag registry.spin.nersc.gov/perssongroup/mp-minio-web:$tag .
+docker image push registry.spin.nersc.gov/perssongroup/mp-minio-web:$tag
 cd $root_dir
